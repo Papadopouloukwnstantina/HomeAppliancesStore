d01c017a6e17350cd5fc9f2cfe733bc6ef191de1
Papadopouloukwnstantina <papadopouloukwn01@gmail.com>
2020-12-06 19:55:25 +0200

    Ask06

--- src/HomeAppliancesStore.java
+++ src/HomeAppliancesStore.java
@@ -1,80 +1,80 @@
-
-interface DeviceBasic{
-
-    void PowerOn ();
-    void PowerOff ();
-    int PeriodicMantainance();
-    boolean Status();
-
-    void SetMikos(int Mikos);
-    void SetIpsos(int ipsos);
-    void SetVathos(int vathos);
-    void SetOnomaKataskeuasth(String OnomaKataskeuasth);
-    void SetOnomaSuskeuhs(String OnomaSuskeuhs);
-    void SetTuposSuskeuhs(String TuposSuskeuhs);
-    void SetKatanalwsh(int Katanalwsh);
-    int GetMikos();
-    int GetIpsos();
-    int GetVathos();
-    String GetOnomaKataskeuasth();
-    String GetOnomaSuskeuhs();
-    String GetTuposSuskeush();
-    int GetKatanalwsh();
-    void Stoixeia();
-}
-
-abstract class Device implements DeviceBasic{
-
-    public int Mikos;
-    public int ipsos;
-    public int vathos;
-    public String OnomaKataskeuasth;
-    public String OnomaSuskeuhs;
-    public String TuposSuskeuhs;
-    public int Katanalwsh;
-
-    // Setters
-    public abstract void SetMikos(int Mikos);
-    public abstract void SetIpsos(int ipsos);
-    public abstract void SetVathos(int vathos);
-    public abstract void SetOnomaKataskeuasth(String OnomaKataskeuasth);
-    public abstract void SetOnomaSuskeuhs(String OnomaSuskeuhs);
-    public abstract void SetTuposSuskeuhs(String TuposSuskeuhs);
-    public abstract void SetKatanalwsh(int Katanalwsh);
-
-    public abstract int GetMikos();
-    public abstract int GetIpsos();
-    public abstract int GetVathos();
-    public abstract String GetOnomaKataskeuasth();
-    public abstract String GetOnomaSuskeuhs();
-    public abstract String GetTuposSuskeush();
-    public abstract int GetKatanalwsh();
-
-    public static boolean status;
-
-    public abstract void Stoixeia();
-}
-
+import java.io.*;
 public class HomeAppliancesStore {
     public static void main(String[] args) {
+    // Suskeues.txt
+    String arxeio = "Suskeues.txt";
 
-    DeviceStatus();
+    // Creating Arrays of Object
+    Fridge FridgeObjects[] = new Fridge[readFile(arxeio , "Fridge")];
+    Oven OvenObjects[] = new Oven[readFile(arxeio , "Oven")];
+    WashineMachine WashingMachineObjects[] = new WashineMachine[readFile(arxeio , "WashineMachine")];
+    AirCondition AirConditionObjects[] = new AirCondition[readFile(arxeio , "AirCondition")];
+
+    // Saving Objects to Arrays
+    for(int i=0; i<FridgeObjects.length; i++){
+        FridgeObjects[i] = new Fridge("Test" , "Test" , "Fridge" , 5 , 5, 5, 5);
+    }
+    for(int i=0; i<OvenObjects.length; i++){
+        OvenObjects[i] = new Oven("Test" , "Test" , "Oven" , 5 , 5, 5, 5);
+    }
+    for(int i=0; i<WashingMachineObjects.length; i++){
+        WashingMachineObjects[i] = new WashineMachine("Test" , "Test" , "WashineMachine" , 5 , 5, 5, 5);
+    }
+    for(int i=0; i<AirConditionObjects.length; i++){
+        AirConditionObjects[i] = new AirCondition("Test" , "Test" , "AirCondition" , 5 , 5, 5, 5);
+    }
+
+    System.out.println(WashineMachine.Suskeuh());
+    System.out.println(Oven.Suskeuh());
+    System.out.println(AirCondition.Suskeuh());
+    System.out.println(Fridge.Suskeuh());
+
+        grapsimosearxeio(FridgeObjects , OvenObjects , WashingMachineObjects , AirConditionObjects);
+    }
+
+    static int readFile(String arxeio , String Suskeuh){
+
+        String grammi;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(arxeio));
+            while ((grammi = br.readLine()) != null) { // While the EOF
+                String[] st = grammi.split(" "); // Array for Holding each Word per Block. Split Words with ' '.
+                String Div = st[0]; // Type of Device.
+                String id = st[1]; // Numbers of Device.
+                // Finding Our Device.
+                if(Div.equals(Suskeuh)){
+                    return Integer.parseInt(st[1]);
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error Corrupted.");
+        }
+
+        System.out.println("Device: " + Suskeuh + " Was not Found\n");
+        return 0;
 
     }
 
-    public static void DeviceStatus(){
+    static public void grapsimosearxeio(Object FridgeObjects[] , Object OvenObjects[] , Object WashingMachineObjects[] , Object AirConditionObject[]){
 
-     Device Div = new Fridge("TEST" , "TEST" , "Fridge" , 2 , 3 , 4 , 5);
+        try{
+            FileOutputStream arxeioout = new FileOutputStream("antikeimena.txt");
+            ObjectOutputStream out = new ObjectOutputStream(arxeioout);
 
-     if(Div instanceof Fridge){
+            out.writeObject(FridgeObjects);
+            out.writeObject(OvenObjects);
+            out.writeObject(WashingMachineObjects);
+            out.writeObject(AirConditionObject);
+            out.close();
 
-         Fridge fridge = (Fridge) Div;
+        }
+        catch(IOException ex){
+            System.out.println("Error Corrupted.");
+        }
 
-         System.out.printf("Device Status: ");
-         if(fridge.Status() == true) System.out.printf("On\n"); else System.out.printf("Off\n");
 
 
-     }
     }
 }
 
--- src/WashineMachine.java
+++ src/WashineMachine.java
@@ -1,84 +1,29 @@
-public class WashineMachine extends Device{
+import java.io.Serializable;
+public class WashineMachine implements Serializable {
 
+    public String OnomaKataskeuasth;
+    public String OnomaSyskeuhs;
+    public String Tupos;
+    public int Mikos;
+    public int Ipsos;
+    public int Vathos;
+    public int Katanalwsh;
+
+    public static int counter = 0;
     // Constructor
     public WashineMachine(String OnomaKataskeuasth , String OnomaSuskeuhs , String TuposSuskeuhs , int Mikos , int Ipsos , int Vathos , int Katanalwsh){
 
-        try{
-            SetMikos(Mikos);
-            SetIpsos(Ipsos);
-            SetVathos(Vathos);
-            SetKatanalwsh(Katanalwsh);
-            SetOnomaSuskeuhs(OnomaSuskeuhs);
-            SetOnomaKataskeuasth(OnomaKataskeuasth);
-            SetTuposSuskeuhs(TuposSuskeuhs);
-        }catch(Exception e){
-            System.out.println("Error: Cant Set Values"); return;
-        }
-
-        Stoixeia();
-    }
-
-
-
-    public boolean Status(){return status;}
-    public void PowerOn(){status = true;}
-    public void PowerOff(){status = false;}
-
-    // Setters
-    public void SetMikos(int Mikos){
-        this.Mikos = Mikos;
-    }
-    public void SetIpsos(int ipsos){
-        this.ipsos = ipsos;
-    }
-    public void SetVathos(int vathos){
-        this.vathos = vathos;
-    }
-    public void SetOnomaKataskeuasth(String OnomaKataskeuasth){
         this.OnomaKataskeuasth = OnomaKataskeuasth;
-    }
-    public void SetOnomaSuskeuhs(String OnomaSuskeuhs){
-        this.OnomaSuskeuhs = OnomaSuskeuhs;
-    }
-    public void SetTuposSuskeuhs(String TuposSuskeuhs){
-        this.TuposSuskeuhs = TuposSuskeuhs;
-    }
-    public void SetKatanalwsh(int Katanalwsh){
+        this.OnomaSyskeuhs = OnomaSuskeuhs;
+        this.Tupos = TuposSuskeuhs;
+        this.Mikos = Mikos;
+        this.Ipsos = Ipsos;
+        this.Vathos = Vathos;
         this.Katanalwsh = Katanalwsh;
+        counter++;
     }
 
-    // Getters
-    public int GetMikos(){
-        return Mikos;
-    }
-    public int GetIpsos(){
-        return ipsos;
-    }
-    public int GetVathos(){
-        return vathos;
-    }
-    public String GetOnomaKataskeuasth(){
-        return OnomaKataskeuasth;
-    }
-    public String GetOnomaSuskeuhs(){return OnomaSuskeuhs;}
-    public String GetTuposSuskeush(){
-        return TuposSuskeuhs;
-    }
-    public int GetKatanalwsh(){
-        return Katanalwsh;
-    }
-
-    public void Stoixeia(){
-        System.out.printf("%s: %s Length: %dcm Height: %dcm Depth: %dcm Consumption: %dkWh/h\n"  , GetTuposSuskeush() , GetOnomaSuskeuhs() , GetMikos() , GetIpsos() , GetVathos() , GetKatanalwsh());
-        System.out.printf("Designer: %s\n" , GetOnomaKataskeuasth());
-
-
-    }
-
-    public int PeriodicMantainance(){
-        return 0;
+    public static String Suskeuh(){
+        return "WashingMachine: " + counter;
     }
 }
-
-
-
